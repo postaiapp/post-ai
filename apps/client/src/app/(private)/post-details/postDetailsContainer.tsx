09a47aab0d9466c0e0aac88184dbe03c1693b11c
Robério Filho <107323497+roberiof@users.noreply.github.com>
2025-04-09 16:40:49 -0300

    (fix): build

--- apps/client/src/app/(private)/post-details/postDetailsContainer.tsx
+++ apps/client/src/app/(private)/post-details/postDetailsContainer.tsx
@@ -1,6 +1,6 @@
 'use client';
 import type React from 'react';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 
 import type { InstagramAccountStore } from '@common/interfaces/instagramAccount';
 import type { PostFormData } from '@common/interfaces/post';
