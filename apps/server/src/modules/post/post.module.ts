a4ef15804462165299e12d5868cd07c2a089e671
ericlbarreto <ericbarreto521@gmail.com>
2025-05-24 22:40:42 -0300

    feat: created sequelize connection

--- apps/server/src/modules/post/post.module.ts
+++ apps/server/src/modules/post/post.module.ts
@@ -1,5 +1,5 @@
 import { EmailService } from '@common/providers/email.service';
-import { DatabaseModule } from '@database/database.module';
+import { DatabaseModule } from '@config/database.module';
 import { InstagramAuthService } from '@modules/instagram-auth/services/instagram-auth.service';
 import { Module } from '@nestjs/common';
 import { Post, PostSchema } from '@schemas/post.schema';
